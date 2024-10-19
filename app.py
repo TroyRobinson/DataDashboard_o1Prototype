2b40824950f47ce21d132a6c9b3bad54b2cd40fe
Troy <troy@lionsrock.com>
2024-10-19 08:12:22 -0500

    Comments Clarification

--- app.py
+++ app.py
@@ -372,5 +372,5 @@ if __name__ == '__main__':
     if not os.path.exists(DATABASE):
         init_db()
     
-    port = int(os.environ.get('PORT', 5000))  # Use PORT provided by Render or default to 5000
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT provided by Render Host or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True) # Configure host for Render
